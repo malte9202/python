1bdd69badb2777c57b1230262b86c66f87d4852e
malte9202 <malte.niedereichholz@gmail.com>
2020-02-22 14:38:29 +0100

    rewrite angle conversion

--- firstProject/angle_conversion.py
+++ firstProject/angle_conversion.py
@@ -1,22 +1,24 @@
 #import pi for calculation
+#catch type errors for invalid input
+
 from math import pi
 
+def angle_conversion(conversion_type, angle_to_convert):
+    if conversion_type == 1:
+        converted_angle = (360 / (2 * pi)) * angle_to_convert
+        print(str(angle_to_convert) + " rad is equal to " + str(converted_angle) + " degree")
+        return converted_angle
+    else:
+        converted_angle = (angle_to_convert / 180) * pi
+        print(str(angle_to_convert) + " degree is equal to " + str(converted_angle) + " rad")
+        return converted_angle
+
+
 conversion_type = int(input("Which type of angle conversion do you need?\n(1) rad to degree\n(2) degree to rad\n"))
-if conversion_type == 1:
-    #convert angle from rad to degree
-    def convert_angle_to_degree():
-        angle_rad = float(input("Please enter the angle in rad "))
-        angle_degree = (360 / (2 * pi)) * angle_rad
-        print("The angle " + str(angle_rad) + " rad is equal to " + str(angle_degree) + " degrees")
+while conversion_type not in [1, 2]:
+    conversion_type = int(input("Please enter 1 or 2 to choose the type of conversion." +
+                                "Which type of angle conversion do you need?\n(1) rad to degree\n(2) degree to rad\n"))
 
-    convert_angle_to_degree()
-elif conversion_type == 2:
-    #convert angle from degree to rad
-    def convert_angle_from_degree():
-        angle_degree = float(input("Please enter the angle in degree "))
-        angle_rad = (angle_degree / 180) * pi
-        print("The angle " + str(angle_degree) + " degree is equal to " + str(angle_rad) + " rad")
+angle_to_convert = float(input("Please enter the angle you want to convert "))
 
-    convert_angle_from_degree()
-else:
-    print("Invalid input, please choose from one of the two conversion types.")
+angle_conversion(conversion_type, angle_to_convert)
--- firstProject/tests.py
+++ firstProject/tests.py
@@ -1,7 +1,3 @@
+from angle_conversion import *
 #test functions
 
-def test_angle_conversion():
-    #test angles rad to degree
-    test_angles_rad = []
-    #test angles degree to rad
-    test_angles_degree = []
