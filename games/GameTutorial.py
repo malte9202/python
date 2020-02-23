6a77ac93bd43b46af97c9f2f84007f1b1a8ef2b4
malte9202 <malte.niedereichholz@gmail.com>
2020-02-23 20:04:24 +0100

    added jump

--- games/GameTutorial.py
+++ games/GameTutorial.py
@@ -15,7 +15,10 @@ x = 50  # x coordinate
 y = 50  # y coordinate
 width = 40  # width
 height = 60  # height
-vel = 10  # movement "speed"
+vel = 15  # movement "speed"
+
+is_jump = False
+jump_count = 10
 
 run = True
 while run:  # game loop
@@ -33,10 +36,24 @@ while run:  # game loop
         x -= vel
     if keys[pygame.K_RIGHT] and x < screen_width - width - vel:  # move right and check for borders
         x += vel
-    if keys[pygame.K_UP] and y < vel:  # move up and check for borders
-        y -= vel
-    if keys[pygame.K_DOWN] and y > screen_width - height - vel:  # move down and check for borders
-        y += vel
+
+    if not(is_jump):
+        if keys[pygame.K_UP] and y > vel:  # move up and check for borders
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screen_width - height - vel:  # move down and check for borders
+            y += vel
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -10:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            y -= (jump_count ** 2) * 0.5 * neg
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 10
 
     win.fill((0, 0, 0))  # fill up former position of the character
     pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))  # draw character
