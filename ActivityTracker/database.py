d4fd15ae877b18a325eb73f420055202a5cada74
malte9202 <malte.niedereichholz@gmail.com>
2020-02-24 22:28:14 +0100

    fixed activities db

--- ActivityTracker/database.py
+++ ActivityTracker/database.py
@@ -1,47 +1,15 @@
 import sqlite3
-from sqlite3 import Error
 
-def create_connection(db_file):
+connection = sqlite3.connect("activity_tracker.db")
+cursor = connection.cursor()
 
-    # create a database connection to SQLite database
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
+create_table_activities = 'CREATE TABLE IF NOT EXISTS activities (id INTEGER PRIMARY KEY, date TEXT, type TEXT, distance REAL, duration REAL, average_speed REAL, additional_info TEXT)'
 
-    return conn
+cursor.execute(create_table_activities)
 
+activity_insert = 'INSERT INTO activities VALUES (?, ?, ?, ?, ?, ?, ?)'
 
-def create_table(conn, create_table_sql):
-    # create a table from the create_table_sql statement
-    # :param conn: Connection object
-    # :param create_table_sql: a CREATE TABLE statement
-    # :return:
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+cursor.execute(activity_insert, (1, '2020-02-24', 'run', 10, 60, 10, 'indoor'))
 
+connection.commit()
 
-def main():
-    database = r"/home/malte/python/ActivityTracker/activities.db"
-
-    sql_create_activities_table = '''CREATE TABLE IF NOT EXISTS activities
-             (id INTEGER PRIMARY KEY, date TEXT, type TEXT, distance REAL, duration REAL, 
-              average_speed REAL, info TEXT)'''
-
-    # create a database connection
-    conn = create_connection(database)
-
-    # create tables
-    if conn is not None:
-        create_table(conn, sql_create_activities_table)
-    else:
-        print("Error! Cannot create the database connection.")
-
-
-if __name__ == '__main__':
-    create_connection(r"/home/malte/python/ActivityTracker/activities.db")
