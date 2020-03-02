80e1946d8b258348d2f55b2ed196edf94b1014d6
malte9202 <malte.niedereichholz@gmail.com>
2020-03-02 19:30:59 +0100

    gui tutorial

---
+++ guiTutorial.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# create a window
+window = Tk()
+
+# set window title
+window.title("title of this window")
+
+# waiting for user input in the main loop
+window.mainloop()
+
+my_button = Button(window, optio)
